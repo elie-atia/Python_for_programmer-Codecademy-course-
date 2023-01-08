51777b295707b848341721ff33ce31af743d69f9
elie-atia <elie-atia@hotmail.fr>
2023-01-08 14:20:13 +0200

    add the codes of the last modules of the courses: built-in data structure

--- 02-control flow/3.pass-break-continue.py
+++ 02-control flow/3.pass-break-continue.py
@@ -1,11 +1,15 @@
-names = ['Amanda', 'Mercedes', 'Rachel', 'Elisabeth', 'Tay', 'Xavier', 'Joaquin', 'Sam']
- 
-for name in names:
-  if 'm' in name.lower():
-      continue
-  elif 'r' in name.lower():
-      pass
-  elif 'j' in name.lower():
-      break
-  else:
-       print(name)
+def main():
+    names = ['Amanda', 'Mercedes', 'Rachel', 'Elisabeth', 'Tay', 'Xavier', 'Joaquin', 'Sam']
+    for name in names:
+        if 'm' in name.lower():
+            continue
+        elif 'r' in name.lower():
+            pass
+            print(name)
+        elif 'j' in name.lower():
+            break
+        else:
+            print(name)
+
+if __name__ == '__main__':
+    main()
